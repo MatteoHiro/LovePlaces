bc26f689d14d4c0ee2b51aeb10a34738f96ecc20
MatteoHiro <matteoo.izzo@gmail.com>
2026-06-28 04:24:31 +0200

    Initial MAUI app setup

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace LovePlaceApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace LovePlaceApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
